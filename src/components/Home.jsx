4f6646efbce4c76d04f201b406f653aecdee189b
Lucas <navarrolucas4668@gmail.com>
2024-09-05 17:55:14 -0300

    agregue el cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="container-padre-home">
+
       <div class="presentacion">
+      <div className="container-cv">
+        <a href="https://drive.google.com/file/d/1l16DaAHHhgVpSSRlYutLGOSy3BSfgkMZ/view?usp=sharing" className="title-cv">cv</a>
+      </div>
         <h1>Lucas Navarro</h1>
         <h4>System Technician</h4>
         {/* <a href="https://drive.google.com/file/d/1Nr7fAYRTzaTIuiQ0iW7_JfUeErpTTJC3/view?usp=sharing">
