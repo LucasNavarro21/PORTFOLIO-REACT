26d56fb32d52d554c5d6b091308c9ebcf9a12242
Lucas <navarrolucas4668@gmail.com>
2024-09-05 07:58:02 -0300

    cambios en la ruta del home

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path = "/" element={<Home/>} />
+          <Route path = "/portfolio-react" element={<Home/>} />
           <Route path = "/about" element={<About/>}/>
           <Route path="/education" element={<Education/>}/>
           <Route path = "/portfolio" element={<Portfolio/>}/>
